3de59c0aafadfcaf07540716440914593a63116c
mourdani <mourdani@student.42.fr>
2022-07-18 16:57:49 +0200

    integrate trigo to movements

--- srcs/move/ft_entry_key.c
+++ srcs/move/ft_entry_key.c
@@ -6,7 +6,7 @@
 /*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 12:45:32 by guderram          #+#    #+#             */
-/*   Updated: 2022/07/17 14:03:10 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/07/18 16:52:31 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@ int		ft_entry_keyboard(int key, void *p)
 	w_point *win;
 
 	win = p;
-	ft_printf("key : %d\nPos_X : %d\nPos_Y : %d\nX : %d\nY : %d\nD : %d\n", key, win->pos_x, win->pos_y, win->x, win->y, (int) win->d);
 	if (key == KEY_ECHAP)
 	{
 		ft_exit_free_all(win, 0);
 		exit(0);
 	}
-	if (key == KEY_MOVE_FRONT || key == KEY_MOVE_LEFT || key == KEY_MOVE_RIGHT || key == KEY_MOVE_BACK || key == KEY_ROTATE_LEFT || key == KEY_ROTATE_RIGHT)
+	if (key == KEY_MOVE_FRONT
+		|| key == KEY_MOVE_LEFT
+		|| key == KEY_MOVE_RIGHT
+		|| key == KEY_MOVE_BACK
+		|| key == KEY_ROTATE_LEFT
+		|| key == KEY_ROTATE_RIGHT)
 	{
 		ft_move_zqsd(key, win);
 		mlx_clear_window(win->mlx, win->win1);
 		ft_reset_ca(win);
 		ft_screen(win); // affiche les déplacements
 	}
+	ft_printf("================\nkey : %d\nPos_X : %d\nPos_Y : %d\nX : %d\nY : %d\nD : %d\n"
+		, key, win->pos_x, win->pos_y, win->x, win->y, (int) win->d);
 	return (0);
 }
--- srcs/move/ft_position.c
+++ srcs/move/ft_position.c
@@ -6,12 +6,13 @@
 /*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 13:30:43 by guderram          #+#    #+#             */
-/*   Updated: 2022/07/17 14:01:40 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/07/18 16:52:27 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
+// prints map.cub
 void    ft_map_disp_pos(w_point *win)
 {
 	int a;
@@ -55,28 +56,75 @@ void	ft_move_in_casa(w_point *win)
 		win->pos_y = 64 - MOVE_SIZE;
 		win->y = win->y - 1;
 	}
-	ft_map_disp_pos(win);
+}
+
+double convert_to_radian(double degree)
+{
+	return(degree * ( M_PI / 180.0 ));
+}
+
+double find_x(double d, double h)
+{
+	return((double)cos((double)convert_to_radian(d)) * h);
+}
+
+double find_y(double d, double h)
+{
+	return((double)sin((double)convert_to_radian(d)) * h);
 }
 
 int		ft_move_zqsd(int key, w_point *win)
 {
-	if (key == KEY_MOVE_FRONT)
-		win->pos_x = win->pos_x - win->move_size;
-	else if (key == KEY_MOVE_LEFT)
-		win->pos_y = win->pos_y - win->move_size;
-	else if (key == KEY_MOVE_RIGHT)
-		win->pos_y = win->pos_y + win->move_size;
-	else if (key == KEY_MOVE_BACK)
-		win->pos_x = win->pos_x + win->move_size;
-	if (win->pos_x > 100 || win->pos_x < 0 || win->pos_y > 100 || win->pos_y < 0)
-		ft_move_in_casa(win);
-	if (key == KEY_ROTATE_LEFT)
-		win->d = win->d - win->d_size;
-	else if (key == KEY_ROTATE_RIGHT)
-		win->d = win->d + win->d_size;
 	if (win->d < 0)
-		win->d = 360 - win->d_size;
+		win->d = 360 - ANG_SIZE;
 	else if (win->d >= 360)
 		win->d = 0;
+	if (win->d == 0)
+	{
+		if (key == KEY_MOVE_FRONT)
+			win->pos_x = win->pos_x - MOVE_SIZE;
+		else if (key == KEY_MOVE_LEFT)
+			win->pos_y = win->pos_y - MOVE_SIZE;
+		else if (key == KEY_MOVE_RIGHT)
+			win->pos_y = win->pos_y + MOVE_SIZE;
+		else if (key == KEY_MOVE_BACK)
+			win->pos_x = win->pos_x + MOVE_SIZE;
+		if (win->pos_x > 100 || win->pos_x < 0 || win->pos_y > 100 || win->pos_y < 0)
+			ft_move_in_casa(win);
+	}
+	else
+	{
+		printf("move size : %d\t\td_size = %d\n", MOVE_SIZE, ANG_SIZE);
+		if (key == KEY_MOVE_FRONT)
+		{
+			win->pos_x -= find_x(win->d, MOVE_SIZE);
+			if (win->d < 180)
+				win->pos_y += find_y(win->d, MOVE_SIZE);
+			if (win->d > 180)
+				win->pos_y -= find_y(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_BACK)
+		{
+			win->pos_x += find_x(win->d, MOVE_SIZE);
+			if (win->d < 180)
+				win->pos_y += find_y(win->d, MOVE_SIZE);
+			if (win->d > 180)
+				win->pos_y -= find_y(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_LEFT)
+		{
+			win->pos_y -= find_y(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_RIGHT)
+		{
+			win->pos_y += find_y(win->d, MOVE_SIZE);
+		}
+		if (win->pos_x > 100 || win->pos_x < 0 || win->pos_y > 100 || win->pos_y < 0)
+			ft_move_in_casa(win);
+	}
+	if (key == KEY_ROTATE_LEFT)
+		win->d = win->d - ANG_SIZE;
+	else if (key == KEY_ROTATE_RIGHT)
+		win->d = win->d + ANG_SIZE;
 	return(0);
 }
