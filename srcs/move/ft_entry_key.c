90468ee6ea34ee0fca3fd43a6e11d0ce42033f42
mourdani <mourdani@student.42.fr>
2022-07-30 21:00:02 +0200

    merge with moves branch

--- srcs/move/ft_entry_key.c
+++ srcs/move/ft_entry_key.c
@@ -6,33 +6,33 @@
 /*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 12:45:32 by guderram          #+#    #+#             */
-/*   Updated: 2022/07/30 13:07:58 by guderram         ###   ########.fr       */
+/*   Updated: 2022/07/19 23:17:51 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../../includes/libft.h"
 
-int	ft_entry_keyboard(int key, void *p)
+int		ft_entry_keyboard(int key, void *p)
 {
-	w_point	*win;
+	w_point *win;
 
 	win = p;
-	printf("key : %d\nPos_X : %d\nPos_Y : %d\nX : %d\nY : %d\nD : %d\n",
-		key, win->pos_x, win->pos_y, win->x, win->y, (int) win->d);
-	if (key == KEY_ECHAP || key == MAC_KEY_ECHAP)
+	if (key == KEY_ECHAP)
 	{
+		ft_exit_free_all(win, 0);
 		exit(0);
 	}
-	if (key == KEY_MOVE_FRONT || key == KEY_MOVE_LEFT
-		|| key == KEY_MOVE_RIGHT || key == KEY_MOVE_BACK
-		|| key == KEY_ROTATE_LEFT || key == KEY_ROTATE_RIGHT
-		|| key == MAC_KEY_MOVE_FRONT || key == MAC_KEY_MOVE_LEFT
-		|| key == MAC_KEY_MOVE_RIGHT || key == MAC_KEY_MOVE_BACK
-		|| key == MAC_KEY_ROTATE_LEFT || key == MAC_KEY_ROTATE_RIGHT)
+	if (key == KEY_MOVE_FRONT
+		|| key == KEY_MOVE_LEFT
+		|| key == KEY_MOVE_RIGHT
+		|| key == KEY_MOVE_BACK
+		|| key == KEY_ROTATE_LEFT
+		|| key == KEY_ROTATE_RIGHT)
 	{
 		ft_move_zqsd(key, win);
 		mlx_clear_window(win->mlx, win->win1);
-		ft_screen(win);
+		ft_reset_ca(win);
+		ft_screen(win); // affiche les déplacements
 	}
 	return (0);
 }
--- srcs/move/ft_position.c
+++ srcs/move/ft_position.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
-/*																			*/
-/*														:::	  ::::::::   */
-/*   ft_position.c									  :+:	  :+:	:+:   */
-/*													+:+ +:+		 +:+	 */
-/*   By: guderram <guderram@student.42.fr>		  +#+  +:+	   +#+		*/
-/*												+#+#+#+#+#+   +#+		   */
-/*   Created: 2022/06/14 13:30:43 by guderram		  #+#	#+#			 */
-/*   Updated: 2022/07/18 17:14:34 by guderram		 ###   ########.fr	   */
-/*																			*/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_position.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guderram <guderram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 13:30:43 by guderram          #+#    #+#             */
+/*   Updated: 2022/07/19 23:20:40 by mourdani         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../../includes/libft.h"
 
-void	ft_map_disp_pos(w_point *win)
+// prints map.cub
+void    ft_map_disp_pos(w_point *win)
 {
-	int	a;
-	int	c;
-
+	int a;
+	int c;
+	
 	a = 0;
 	c = 0;
-	printf("pos : \n");
+	ft_printf("pos : \n");
 	while (win->map->map[a] != NULL)
 	{
 		while (win->map->map[a][c] != '\0')
 		{
-			printf("\033[0m%c", win->map->map[a][c]);
+			ft_printf("\033[0m%c", win->map->map[a][c]);
 			c++;
 		}
-		printf("\n");
+		ft_printf("\n");
 		c = 0;
 		a++;
 	}
@@ -55,29 +56,57 @@ void	ft_move_in_casa(w_point *win)
 		win->pos_y = 64 - MOVE_SIZE;
 		win->y = win->y - 1;
 	}
-	ft_map_disp_pos(win);
 }
 
-int	ft_move_zqsd(int key, w_point *win)
+int		ft_move_zqsd(int key, w_point *win)
 {
-	if (key == KEY_MOVE_FRONT || key == MAC_KEY_MOVE_FRONT)
-		win->pos_x = win->pos_x - MOVE_SIZE;
-	if (key == KEY_MOVE_LEFT || key == MAC_KEY_MOVE_LEFT)
-		win->pos_y = win->pos_y - MOVE_SIZE;
-	if (key == KEY_MOVE_RIGHT || key == MAC_KEY_MOVE_RIGHT)
-		win->pos_y = win->pos_y + MOVE_SIZE;
-	if (key == KEY_MOVE_BACK || key == MAC_KEY_MOVE_BACK)
-		win->pos_x = win->pos_x + MOVE_SIZE;
-	if (win->pos_x > 100 || win->pos_x < 0
-		|| win->pos_y > 100 || win->pos_y < 0)
-		ft_move_in_casa(win);
-	if (key == KEY_ROTATE_LEFT || key == MAC_KEY_ROTATE_LEFT)
+	if (key == KEY_ROTATE_LEFT)
 		win->d = win->d - ANG_SIZE;
-	if (key == KEY_ROTATE_RIGHT || key == MAC_KEY_ROTATE_RIGHT)
+	else if (key == KEY_ROTATE_RIGHT)
 		win->d = win->d + ANG_SIZE;
 	if (win->d < 0)
 		win->d = 360 - ANG_SIZE;
-	if (win->d >= 360)
+	else if (win->d >= 360)
 		win->d = 0;
-	return (0);
+	if (win->d == 0)
+	{
+		if (key == KEY_MOVE_FRONT)
+			win->pos_y = win->pos_y + MOVE_SIZE;
+		else if (key == KEY_MOVE_BACK)
+			win->pos_y = win->pos_y - MOVE_SIZE;
+		else if (key == KEY_MOVE_RIGHT)
+			win->pos_x = win->pos_x + MOVE_SIZE;
+		else if (key == KEY_MOVE_LEFT)
+			win->pos_x = win->pos_x - MOVE_SIZE;
+		if (win->pos_y > 100 || win->pos_y < 0 || win->pos_x > 100 || win->pos_x < 0)
+			ft_move_in_casa(win);
+	}
+	else
+	{
+		if (key == KEY_MOVE_FRONT)
+		{
+			win->pos_y += find_y(win->d, MOVE_SIZE);
+			win->pos_x += find_x(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_BACK)
+		{
+			win->pos_y -= find_y(win->d, MOVE_SIZE);
+			win->pos_x -= find_x(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_LEFT)
+		{
+			win->pos_y += find_x(win->d, MOVE_SIZE);
+			win->pos_x += find_y(win->d, MOVE_SIZE);
+		}
+		else if (key == KEY_MOVE_RIGHT)
+		{
+			win->pos_y -= find_x(win->d, MOVE_SIZE);
+			win->pos_x -= find_y(win->d, MOVE_SIZE);
+		}
+		if (win->pos_y > 100 || win->pos_y < 0 || win->pos_x > 100 || win->pos_x < 0)
+			ft_move_in_casa(win);
+	}
+	ft_printf("================\nkey : %d\nPos_X : %d\nPos_Y : %d\nX : %d\nY : %d\nD : %d\n"
+		, key, win->pos_x, win->pos_y, win->x, win->y, (int) win->d);
+	return(0);
 }
